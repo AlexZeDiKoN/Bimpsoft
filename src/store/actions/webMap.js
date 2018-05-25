f50e506f78319b2fbc525f6de7c11cbac1dd26b2
serhii.hryb <serhii.hryb@softline.kiev.ua>
2018-05-25 12:41:39 +0300

    Intermediate commit.

--- src/store/actions/webMap.js
+++ src/store/actions/webMap.js
@@ -1,8 +1,26 @@
 export const actionNames = {
   TOGGLE_MAP_EDIT_MODE: Symbol('TOGGLE_MAP_EDIT_MODE'),
   TOGGLE_POINT_MARK_EDIT_MODE: Symbol('TOGGLE_POINT_MARK_EDIT_MODE'),
+  TOGGLE_TEXT_MARK_EDIT_MODE: Symbol('TOGGLE_TEXT_MARK_EDIT_MODE'),
+  TOGGLE_TIMELINE_EDIT_MODE: Symbol('TOGGLE_TIMELINE_EDIT_MODE'),
 }
 
-export const toggleMapEditMode = () => ({ type: actionNames.TOGGLE_MAP_EDIT_MODE })
+export const toggleMapEditMode = (value) => ({
+  type: actionNames.TOGGLE_MAP_EDIT_MODE,
+  payload: value,
+})
 
-export const togglePointMarkEditMode = () => ({ type: actionNames.TOGGLE_POINT_MARK_EDIT_MODE })
+export const togglePointMarkEditMode = (value) => ({
+  type: actionNames.TOGGLE_POINT_MARK_EDIT_MODE,
+  payload: value,
+})
+
+export const toggleTextMarkEditMode = (value) => ({
+  type: actionNames.TOGGLE_TEXT_MARK_EDIT_MODE,
+  payload: value,
+})
+
+export const toggleTimelineEditMode = (value) => ({
+  type: actionNames.TOGGLE_TIMELINE_EDIT_MODE,
+  payload: value,
+})
--- src/store/reducers/webMap.js
+++ src/store/reducers/webMap.js
@@ -4,16 +4,46 @@ import { actionNames } from '../actions/webMap'
 
 const WebMapState = ImmutableRecord({
   isMapEditMode: false,
-  isPointMarkMode: false,
+  isPointMarkEditMode: false,
+  isTextMarkEditMode: false,
+  isTimelineEditMode: false,
 })
 
 export default function webMapReducer (state = new WebMapState(), action) {
-  const { type } = action
+  let { type, payload } = action
   switch (type) {
     case actionNames.TOGGLE_MAP_EDIT_MODE:
-      return state.set('isMapEditMode', !state.get('isMapEditMode'))
+      if (!payload) {
+        payload = !state.get('isMapEditMode')
+      }
+      return state.merge({
+        isMapEditMode: payload,
+        isPointMarkEditMode: false,
+        isTextMarkEditMode: false,
+      })
     case actionNames.TOGGLE_POINT_MARK_EDIT_MODE:
-      return state.set('isPointMarkMode', !state.get('isPointMarkMode'))
+      if (!payload) {
+        payload = !state.get('isPointMarkEditMode')
+      }
+      return state.merge({
+        isMapEditMode: false,
+        isPointMarkEditMode: payload,
+        isTextMarkEditMode: false,
+      })
+    case actionNames.TOGGLE_TEXT_MARK_EDIT_MODE:
+      if (!payload) {
+        payload = !state.get('isTextMarkEditMode')
+      }
+      return state.merge({
+        isMapEditMode: false,
+        isPointMarkEditMode: false,
+        isTextMarkEditMode: payload,
+      })
+    case actionNames.TOGGLE_TIMELINE_EDIT_MODE:
+      if (!payload) {
+        payload = !state.get('isTimelineEditMode')
+      }
+      return state.set('isTimelineEditMode', payload)
     default:
       return state
   }
@@ -21,4 +51,7 @@ export default function webMapReducer (state = new WebMapState(), action) {
 
 export const propTypes = PropTypes.shape({
   isMapEditMode: PropTypes.bool.isRequired,
+  isPointMarkEditMode: PropTypes.bool.isRequired,
+  isTextMarkEditMode: PropTypes.bool.isRequired,
+  isTimelineEditMode: PropTypes.bool.isRequired,
 })
