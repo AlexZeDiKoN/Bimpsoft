870f2c87f082a745aa64efbfa2a6a6e05c3d1fd2
Anatolii Samoilenko <anatolii.samoilenko@softline.kiev.ua>
2018-08-21 19:53:07 +0300

    fix subordination level for milsymbols

--- src/components/WebMap/index.js
+++ src/components/WebMap/index.js
@@ -353,7 +353,7 @@ export default class WebMap extends Component {
     if (this.map) {
       this.map.eachLayer((layer) => {
         if (layer.id && layer.object) {
-          const layerLevel = Math.max(layer.object.level, SubordinationLevel.list[0].number)
+          const layerLevel = Math.max(layer.object.level, SubordinationLevel.TEAM_CREW)
           layer.getElement().style.display = layerLevel < level ? 'none' : ''
         }
       })
@@ -513,14 +513,14 @@ export default class WebMap extends Component {
   createPointSign = async (data) => {
     // console.log('createPointSign', data)
     const { addObject } = this.props
-    const { code, amplifiers, coordinates: p } = data
+    const { code, amplifiers, subordinationLevel = 0, coordinates: p } = data
     const point = { lat: p.lat, lng: p.lng }
     const created = await addObject({
       type: entityKind.POINT,
       code,
       amplifiers,
       point,
-      level: +code.slice(8, 10) || 0,
+      level: +subordinationLevel,
       layer: this.props.layer,
       geometry: [ point ],
     })
@@ -538,7 +538,7 @@ export default class WebMap extends Component {
   }
 
   updatePointSign = async (data) => {
-    const { id, code, coordinates, coordinatesArray, amplifiers, ...rest } = data
+    const { id, code, coordinates, coordinatesArray, amplifiers, subordinationLevel = 0, ...rest } = data
     const point = { lng: +coordinates.lng, lat: +coordinates.lat }
     const layer = this.findLayerById(id)
     if (layer) {
@@ -553,7 +553,7 @@ export default class WebMap extends Component {
       layer: layer.object.layer,
       geometry: [ point ],
       ...rest,
-      level: +code.slice(8, 10) || 0,
+      level: +subordinationLevel,
     })
     this.activateCreated(id)
     // TODO: скинути дані в сторі
