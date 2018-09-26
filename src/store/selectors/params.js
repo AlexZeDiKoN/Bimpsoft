3c58c06e5b91db05fc586f9848c39a2acfec459c
tumak.aleksandr <tumak.aleksandr@iqusion.com>
2018-09-26 18:43:23 +0300

    = fix

--- src/store/selectors/params.js
+++ src/store/selectors/params.js
@@ -5,5 +5,5 @@ const getParam = (name) => (state) => state.params[name]
 
 export const pointSizesSelector = createSelector(
   [ getParam(POINT_SIZE_MIN), getParam(POINT_SIZE_MAX) ],
-  (min, max) => ({ min, max })
+  (min, max) => ({ min: +min, max: +max })
 )
