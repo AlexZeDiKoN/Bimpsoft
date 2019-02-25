b5661fb6d46024795e28d98e9f94990dbda71358
Illia Shevchuk <illia.shevchuk@softline.kiev.ua>
2019-02-25 12:56:27 +0200

    SP

--- src/components/common/March/index.js
+++ src/components/common/March/index.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Form, Icon, Input } from 'antd'
+import { components } from '@DZVIN/CommonComponents'
+import i18n from '../../../i18n'
 import './style.css'
 
+const { IconHovered } = components.icons
+
 class March extends Component {
+  static propTypes = {
+    form: PropTypes.object.isRequired,
+  }
+
   render () {
+    const {
+      form: { getFieldDecorator },
+    } = this.props
+    const { FormRow } = components.form
     return (
-      <div>
-        THERE WILL BE A FORM OF MARCH...
+      <div className='march_container'>
+        <div className='march_title'>
+          {i18n.MARCH_TITLE}
+        </div>
+        <Form>
+          <div className='march_name'>
+            <div className='march_name-indicator'>
+              <Icon type="branches" />
+            </div>
+            <div className='march_name-form'>
+              <FormRow className='march_name-title'>
+                <Input placeholder='placeholder' />
+              </FormRow>
+            </div>
+            <div className='march_name-load'> </div>
+          </div>
+        </Form>
       </div>
     )
   }
 }
 
-const WrappedMarch = March
+const WrappedMarch = Form.create()(March)
 
 export default WrappedMarch
---
+++ src/constants/March.js
@@ -0,0 +1,3 @@
+export const MARCH_KEYS = {
+
+}
