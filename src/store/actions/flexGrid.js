4b3c204c5befc863dbcfe90405c1c832057c47d8
E.Tulyakov <yevhenii.tuliakov@softline.kiev.ua>
2019-04-11 14:16:02 +0300

    resolve discussion: deleted excess actionName

--- src/store/actions/flexGrid.js
+++ src/store/actions/flexGrid.js
@@ -16,7 +16,6 @@ export const SET_ZONES = action('SET_ZONES')
 export const CLOSE_FLEX_GRID_FORM = action('CLOSE_FLEX_GRID_FORM')
 export const FLEX_GRID_DELETED = action('FLEX_GRID_DELETED')
 export const GET_FLEXGRID = action('GET_FLEXGRID')
-export const SET_DIR_NAME = action('SET_DIR_NAME')
 export const SELECT_DIRECTION = action('SELECT_DIRECTION')
 export const DESELECT_DIRECTION = action('DESELECT_DIRECTION')
 
