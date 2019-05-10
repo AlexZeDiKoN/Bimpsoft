a9d880f9e00991ac8f25ae62f9f86ca9e1b97924
illia.shevchuk <illia.shevchuk@softline.kiev.ua>
2019-05-10 10:51:19 +0300

    FF

--- src/components/OrgStructuresComponent/children/OrgStructureMenu/index.js
+++ src/components/OrgStructuresComponent/children/OrgStructureMenu/index.js
@@ -4,6 +4,7 @@ import { Menu, Dropdown } from 'antd'
 import PropTypes from 'prop-types'
 import { components } from '@DZVIN/CommonComponents'
 import i18n from '../../../../i18n'
+import entityKind from '../../../../constants/SelectionTypes'
 
 const {
   icons: { names: iconNames, IconButton },
@@ -18,7 +19,7 @@ export default class Index extends React.Component {
   selectUnboundObjects = () => {
     const { onMapObjects, selectList } = this.props
     const unboundObjects = onMapObjects.toArray()
-      .filter((item) => item.type === 1 && !item.unit)
+      .filter((item) => item.type === entityKind.POINT && !item.unit)
       .map((item) => item.id)
     selectList(unboundObjects)
   }
