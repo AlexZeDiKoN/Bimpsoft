f3908a5a19b4a64e2ba19b86962dfec9e0dd92a4
E.Tulyakov <yevhenii.tuliakov@softline.kiev.ua>
2019-07-25 18:46:39 +0300

    added if else rendering condition

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-        { plugin: require('craco-plugin-react-hot-reload') },
-        { plugin: require('craco-cesium')() }
-    ]
+  plugins: [
+    { plugin: require('craco-plugin-react-hot-reload') },
+    { plugin: require('craco-cesium')() },
+  ],
 }
--- src/layouts/ApplicationContent/index.js
+++ src/layouts/ApplicationContent/index.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { WebMap, PrintGrid, PrintLegendContainer } from '../../containers'
+import { volumeMap as key3D } from '../../constants/viewModesKeys'
 import { MapConsumer } from '../../components/WebMap/MapContext'
+import WebMap3D from '../../components/WebMap3D'
 
 class ApplicationContent extends React.PureComponent {
+  static propTypes = {
+    is3DMapMode: PropTypes.bool.isRequired,
+  }
+
   render () {
+    const { is3DMapMode } = this.props
     return (
-      <WebMap
-        center={[ 48.5, 38 ]}
-        zoom={14}
-      >
-        <MapConsumer>{(map) => map && <PrintGrid map={map} />}</MapConsumer>
-        <PrintLegendContainer />
-      </WebMap>
+      <>
+        {
+          is3DMapMode
+            ? <WebMap3D />
+            : <WebMap
+              center={[ 48.5, 38 ]}
+              zoom={14}
+            >
+              <MapConsumer>{(map) => map && <PrintGrid map={map} />}</MapConsumer>
+              <PrintLegendContainer />
+            </WebMap>
+        }
+      </>
+
     )
   }
 }
 
-export default ApplicationContent
+const mapStateToProps = (store) => ({
+  is3DMapMode: store[key3D],
+})
+
+export default connect(mapStateToProps)(ApplicationContent)
