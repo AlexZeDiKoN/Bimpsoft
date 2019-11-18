295eb7a9746084d05e3de8799646fba39d2543ac
Konstantin Skrypak <kostiantyn.skrypak@iqusion.com>
2019-11-18 16:41:46 +0200

    change reducer

--- src/store/reducers/march.js
+++ src/store/reducers/march.js
@@ -1,5 +1,6 @@
-import { omit, remove, update, insert, compose } from 'ramda'
+import { omit, remove, update, insert, flatten, pick, compose } from 'ramda'
 import { march } from '../actions'
+import { MARCH_SEGMENT_KEYS } from '../../constants/March'
 
 const initState = {
   marchEdit: false,
@@ -10,14 +11,14 @@ const initState = {
 }
 
 // eslint-disable-next-line
-const uuid = () => ([ 1e7 ] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ crypto.getRandomValues(new Uint8Array(1))[ 0 ] & 15 >> c / 4).toString(16))
+const uuid = () => ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16))
 
 export default function reducer (state = initState, action) {
   const { type, payload } = action
 
   switch (type) {
     case march.GET_TYPE_KINDS: {
-      const indicators = payload.reduce((prev, current) => ({ ...prev, [ current.typeCode ]: current }), {})
+      const indicators = payload.reduce((prev, current) => ({ ...prev, [current.typeCode]: current }), {})
       return { ...state, indicators }
     }
     case march.SET_MARCH_PARAMS: {
@@ -33,36 +34,38 @@ export default function reducer (state = initState, action) {
     }
     case march.ADD_POINT: {
       const { segments } = state.params
-      const position = payload === 1 ? payload + 1 : payload
-      const template = { ...segments[ 0 ].complementarySegment }
-      const updatedSegments = insert(position, {
-        ...template,
-        id: uuid(),
-        coordinateFinish: segments[ payload ].coordinateFinish,
-        landmarkFinish: segments[ payload ].landmarkFinish,
-      }, segments)
+      const { index, optional } = payload
+      const position = index === 2 ? index + 1 : index
 
-      const defaultSegmentData = { default: updatedSegments[ payload ].default, id: updatedSegments[ payload ].id }
-      updatedSegments.splice(payload, 1, defaultSegmentData)
+      const nextSegment = pick([ 'required', 'possibleTypes' ], segments[position])
 
-      const params = {
-        ...state.params,
-        segments: updatedSegments,
-      }
+      const options = optional.map((val) => ({ ...val, id: uuid() }))
+      const updatedSegments = compose(
+        flatten,
+        insert(position, options),
+        update(position, nextSegment),
+      )(segments)
+      const params = { ...state.params, segments: updatedSegments }
 
       return { ...state, params }
     }
     case march.DELETE_POINT: {
       const { segments } = state.params
       const nextSegmentIndex = payload + 1
-      const nextSegment = omit([ 'segment', 'segmentName', 'segmentType' ], segments[nextSegmentIndex])
-      const updatedSegments = compose(remove(payload, 1), update(nextSegmentIndex, nextSegment))(segments)
+      const previousSegmentIndex = payload - 1
+
+      const nextSegment = pick([ 'required', 'possibleTypes' ], segments[nextSegmentIndex])
+
+      const updatedSegments = compose(
+        remove(previousSegmentIndex, 2),
+        update(nextSegmentIndex, nextSegment),
+      )(segments)
       const params = { ...state.params, segments: updatedSegments }
       return { ...state, params }
     }
     case march.DELETE_SEGMENT: {
       const { segments } = state.params
-      const updatedSegment = omit([ 'segment', 'segmentName' ], segments[payload])
+      const updatedSegment = omit([ MARCH_SEGMENT_KEYS.SEGMENT, MARCH_SEGMENT_KEYS.SEGMENT_NAME ], segments[payload])
       const updatedSegments = update(payload, updatedSegment, segments)
       const params = { ...state.params, segments: updatedSegments }
       return { ...state, params }
