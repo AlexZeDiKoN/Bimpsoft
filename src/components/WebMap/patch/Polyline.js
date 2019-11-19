496a995d3fd4fcddc2fa57b95da65eecfb3da26d
yevhen.kyslomed <yevhen.kyslomed@softline.kiev.ua>
2019-11-19 11:54:35 +0200

    remove simplification of curve and area (DZVIN-5163)

--- src/components/WebMap/patch/Polyline.js
+++ src/components/WebMap/patch/Polyline.js
@@ -6,7 +6,7 @@ const { _simplifyPoints } = L.Polyline.prototype
 L.Polyline.include({
   _simplifyPoints: function () {
     const kind = this.options && this.options.tsType
-    if (kind !== entityKind.AREA && kind === entityKind.CURVE) {
+    if (kind !== entityKind.AREA && kind !== entityKind.CURVE) {
       _simplifyPoints.call(this)
     }
   },
