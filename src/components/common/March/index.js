7b7e80b71cf929cbecd684aeb0d9d779b2ee0e5a
Konstantin Skrypak <kostiantyn.skrypak@iqusion.com>
2019-11-20 11:36:54 +0200

    fix spaces

--- src/components/common/March/index.js
+++ src/components/common/March/index.js
@@ -10,136 +10,140 @@ import SegmentContainer from './children/Segment'
 const { names: iconNames, IconButton } = components.icons
 
 class March extends Component {
-    static propTypes = {
-      form: PropTypes.object.isRequired,
-      indicators: PropTypes.object,
-      params: PropTypes.object,
-      integrity: PropTypes.bool,
-      setMarchParams: PropTypes.func,
-      setIntegrity: PropTypes.func,
+  static propTypes = {
+    form: PropTypes.object.isRequired,
+    indicators: PropTypes.object,
+    params: PropTypes.object,
+    integrity: PropTypes.bool,
+    setMarchParams: PropTypes.func,
+    setIntegrity: PropTypes.func,
+  }
+
+  indicatorItemObj = (target, indicator) => {
+    const activeItem = indicator.typeValues.filter((item) => item.id === Number(target))
+    return {
+      indicatorId: indicator.id,
+      id: activeItem[ 0 ].id,
+      name: activeItem[ 0 ].name,
     }
+  }
 
-    indicatorItemObj = (target, indicator) => {
-      const activeItem = indicator.typeValues.filter((item) => item.id === Number(target))
-      return {
-        indicatorId: indicator.id,
-        id: activeItem[ 0 ].id,
-        name: activeItem[ 0 ].name,
-      }
-    }
+  handleMarchType = (target, key) => {
+    const { setMarchParams, indicators } = this.props
+    const { MARCH_TEMPLATES, MARCH_INDICATORS_GROUP } = MarchKeys
+    const template = MARCH_TEMPLATES[ target ].required
+    const targetObj = this.indicatorItemObj(target,
+      indicators[ MARCH_INDICATORS_GROUP.movementType ])
+    setMarchParams({
+      [ key ]: targetObj,
+      template,
+    })
+  }
 
-    handleMarchType = (target, key) => {
-      const { setMarchParams, indicators } = this.props
-      const { MARCH_TEMPLATES, MARCH_INDICATORS_GROUP } = MarchKeys
-      const template = MARCH_TEMPLATES[ target ].required
-      const targetObj = this.indicatorItemObj(target, indicators[MARCH_INDICATORS_GROUP.movementType])
-      setMarchParams({
-        [key]: targetObj,
-        template,
-      })
-    }
+  createSelectChildren = (incomeData) => incomeData
+    .map((item) => <Select.Option key={item.id}>{item.name}</Select.Option>)
 
-    createSelectChildren = (incomeData) => incomeData
-      .map((item) => <Select.Option key={item.id}>{item.name}</Select.Option>)
+  checkIntegrityOfMarch = () => {
+    const { form, setIntegrity } = this.props
+    form.validateFields((err) => setIntegrity(!err))
+  }
 
-    checkIntegrityOfMarch = () => {
-      const { form, setIntegrity } = this.props
-      form.validateFields((err) => setIntegrity(!err))
-    }
-
-    render () {
-      const {
-        form,
-        form: { getFieldDecorator },
-        indicators,
-        setMarchParams,
-        params: { segments },
-        integrity,
-      } = this.props
-      const { FormRow } = components.form
-      const { MARCH_KEYS, MARCH_INDICATORS_GROUP } = MarchKeys
-      return (
-        <div className="march_container">
-          <div className="march_title">{i18n.MARCH_TITLE}</div>
-          <div className="march_link-bl">
-            <a href="#/" title="Варіант маршруту">
-              {i18n.MARCH_LINK} -{' '}
-            </a>
-          </div>
-          {indicators && (
-            <Form
-              className="march_form"
-              onBlur={this.checkIntegrityOfMarch}
-            >
-              <div className="march_name">
-                <div className="march_name-indicator">
-                  <Icon type="branches" className={`march_icon-${integrity ? 'success' : 'error'}`} />
-                </div>
-                <div className="march_name-form">
-                  <FormRow>
-                    {getFieldDecorator(MARCH_KEYS.MARCH_NAME)(
-                      <Input
-                        className="march_name-title"
-                        placeholder={i18n.MARCH_NAME}
-                        onChange={({ target }) =>
-                          setMarchParams({
-                            [MARCH_KEYS.MARCH_NAME]: target.value,
-                          })
-                        }
-                      />,
-                    )}
-                  </FormRow>
-                  <FormRow>
-                    {getFieldDecorator(MARCH_KEYS.MARCH_TYPE)(
-                      <Select
-                        placeholder={i18n.MARCH_TYPE}
-                        onChange={(e) =>
-                          this.handleMarchType(e, MARCH_KEYS.MARCH_TYPE)
-                        }
-                      >
-                        {this.createSelectChildren(
-                          indicators[MARCH_INDICATORS_GROUP.movementType].typeValues,
-                        )}
-                      </Select>,
-                    )}
-                  </FormRow>
-                </div>
-                <div className="march_name-load">
-                  <IconButton
-                    title={i18n.OPEN_MARCH_FILE}
-                    icon={iconNames.PACK_DEFAULT}
-                    hoverIcon={iconNames.PACK_HOVER}
-                    onClick={() => console.info('open file')}
-                  />
-                </div>
-              </div>
-              <div className="march_track">
-                {segments.map((item, i) => (
-                  <SegmentContainer key={item.id || i} index={i} form={form} />
-                ))}
-              </div>
-              <div className="march_total_distance">{i18n.MARCH_DISTANCE}: {0} км</div>
-              <div className="march_buttonBlock">
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="march_button-submit"
-                >
-                  {i18n.CREATE_BTN_TITLE}
-                </Button>
-                <Button
-                  htmlType="reset"
-                  className="march_button-cancel"
-                  onClick={() => console.info('cancel')}
-                >
-                  {i18n.CANCEL_BTN_TITLE}
-                </Button>
-              </div>
-            </Form>
-          )}
+  render () {
+    const {
+      form,
+      form: { getFieldDecorator },
+      indicators,
+      setMarchParams,
+      params: { segments },
+      integrity,
+    } = this.props
+    const { FormRow } = components.form
+    const { MARCH_KEYS, MARCH_INDICATORS_GROUP } = MarchKeys
+    return (
+      <div className="march_container">
+        <div className="march_title">{i18n.MARCH_TITLE}</div>
+        <div className="march_link-bl">
+          <a href="#/" title="Варіант маршруту">
+            {i18n.MARCH_LINK} -{' '}
+          </a>
         </div>
-      )
-    }
+        {indicators && (
+          <Form
+            className="march_form"
+            onBlur={this.checkIntegrityOfMarch}
+          >
+            <div className="march_name">
+              <div className="march_name-indicator">
+                <Icon type="branches"
+                  className={`march_icon-${integrity ? 'success' : 'error'}`}/>
+              </div>
+              <div className="march_name-form">
+                <FormRow>
+                  {getFieldDecorator(MARCH_KEYS.MARCH_NAME,
+                    { rules: [ { required: true } ] })(
+                    <Input
+                      className="march_name-title"
+                      placeholder={i18n.MARCH_NAME}
+                      onChange={({ target }) =>
+                        setMarchParams({
+                          [ MARCH_KEYS.MARCH_NAME ]: target.value,
+                        })
+                      }
+                    />,
+                  )}
+                </FormRow>
+                <FormRow>
+                  {getFieldDecorator(MARCH_KEYS.MARCH_TYPE,
+                    { rules: [ { required: true } ] })(
+                    <Select
+                      placeholder={i18n.MARCH_TYPE}
+                      onChange={(e) =>
+                        this.handleMarchType(e, MARCH_KEYS.MARCH_TYPE)
+                      }
+                    >
+                      {this.createSelectChildren(
+                        indicators[ MARCH_INDICATORS_GROUP.movementType ].typeValues,
+                      )}
+                    </Select>,
+                  )}
+                </FormRow>
+              </div>
+              <div className="march_name-load">
+                <IconButton
+                  title={i18n.OPEN_MARCH_FILE}
+                  icon={iconNames.PACK_DEFAULT}
+                  hoverIcon={iconNames.PACK_HOVER}
+                  onClick={() => console.info('open file')}
+                />
+              </div>
+            </div>
+            <div className="march_track">
+              {segments.map((item, i) => (
+                <SegmentContainer key={item.id || i} index={i} form={form}/>
+              ))}
+            </div>
+            <div className="march_total_distance">{i18n.MARCH_DISTANCE}: {0} км</div>
+            <div className="march_buttonBlock">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="march_button-submit"
+              >
+                {i18n.CREATE_BTN_TITLE}
+              </Button>
+              <Button
+                htmlType="reset"
+                className="march_button-cancel"
+                onClick={() => console.info('cancel')}
+              >
+                {i18n.CANCEL_BTN_TITLE}
+              </Button>
+            </div>
+          </Form>
+        )}
+      </div>
+    )
+  }
 }
 
 const WrappedMarch = Form.create()(March)
