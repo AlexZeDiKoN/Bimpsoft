9f3f01d2c9f7ad607765e21b83c589155ec0f452
Andrey Vereshchak <andrii.vereshchak@softline.kiev.ua>
2019-12-05 12:01:07 +0200

    Add optional-chaining nad nullish-coalescing-operator babel plugins

--- craco.config.js
+++ craco.config.js
@@ -3,4 +3,10 @@ module.exports = {
     { plugin: require('craco-plugin-react-hot-reload') },
     { plugin: require('craco-cesium')() },
   ],
+  babel: {
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+    ],
+  },
 }
