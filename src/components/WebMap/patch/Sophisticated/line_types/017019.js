32c0541e1c18d18f22889553c124a5325df4e23d
oleksii.samoilenko <oleksii.samoilenko@iqusion.com>
2020-04-10 16:21:32 +0300

    вывод Круговых зон с заливкой выбранным цветом.

--- src/components/WebMap/patch/Sophisticated/line_types/017019.js
+++ src/components/WebMap/patch/Sophisticated/line_types/017019.js
@@ -1,4 +1,4 @@
-import { utils } from '@DZVIN/CommonComponents'
+import { Earth } from 'leaflet/src/geo/crs/CRS.Earth'
 import { MIDDLE, DELETE, STRATEGY } from '../strategies'
 import lineDefinitions from '../lineDefinitions'
 import {
@@ -7,9 +7,7 @@ import {
 import {
   lengthLine, isDefPoint,
 } from '../arrowLib'
-import { distanceAzimuth } from '../../utils/sectors'
-
-const { Coordinates: Coord } = utils
+import { colors } from '../../../../../constants'
 
 // sign name: Дальність дії (кругові)
 // task code: DZVIN-5769 (part 3)
@@ -49,26 +47,24 @@ lineDefinitions['017019'] = {
       return
     }
     const pO = points[0]
+    let pP = pO
+    let radiusP = 0
     const pgO = coordArray[0]
     points.forEach((elm, ind) => {
-      if (isDefPoint(elm)) {
+      if (isDefPoint(elm) && (ind > 0)) {
         const radius = lengthLine(pO, elm)
         const color = sectorsInfo[ind]?.color ?? COLORS[ind]
+        const fillColor = colors.values[sectorsInfo[ind]?.fill] ?? 'transparent'
         drawCircle(result, pO, radius + !ind * 2)
         result.amplifiers += `<circle stroke-width="${width}" stroke="${color}" fill="transparent" cx="${pO.x}" cy="${pO.y}" r="${radius}"/> `
-        let radiusM
-        if (ind !== 0) {
-          if (!Coord.check(coordArray[ind])) {
-            radiusM = 0
-          } else {
-            radiusM = distanceAzimuth(pgO, coordArray[ind]).distance.toFixed(0)
-          }
-          // const m = Math.round(result.layer._map.layerPointToLatLng(pO)
-          // .distanceTo(result.layer._map.layerPointToLatLng(elm)))
-          const amplifier = sectorsInfo[ind]?.amplifier ?? '_'
-          drawText(result, { x: elm.x, y: elm.y }, 0, radiusM, SMALL_TEXT_SIZE, 'middle', null, 'after-edge')
-          drawText(result, { x: elm.x, y: elm.y }, 0, amplifier, 1, 'middle', null, 'before-edge')
-        }
+        result.amplifiers += `<path fill-rule="evenodd" stroke-width="0" fill="${fillColor}" fill-rule="nonzero" fill-opacity="0.25" 
+            d="M${elm.x} ${elm.y} a${radius} ${radius} 0 1 1 0.01 0 M${pP.x} ${pP.y} a${radiusP} ${radiusP} 0 1 1 0.01 0"/>`
+        radiusP = radius
+        pP = elm
+        const radiusM = Earth.distance(pgO, coordArray[ind]).toFixed(0)
+        const amplifier = sectorsInfo[ind]?.amplifier ?? ''
+        drawText(result, { x: elm.x, y: elm.y }, 0, radiusM, SMALL_TEXT_SIZE, 'middle', null, 'after-edge')
+        drawText(result, { x: elm.x, y: elm.y }, 0, amplifier, 1, 'middle', null, 'before-edge')
       }
     })
   },
