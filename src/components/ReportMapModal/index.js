3e6575b891a6cd29385e9f549818d93eeffe2059
yurii.khomenko <y.khomenko@iqusion.com>
2020-05-19 13:42:51 +0300

    added popup window

--- src/components/ReportMapModal/index.js
+++ src/components/ReportMapModal/index.js
@@ -2,12 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import FocusTrap from 'react-focus-lock'
 import { MovablePanel } from '@DZVIN/CommonComponents'
-import { Select, Input, Button } from 'antd'
+import { Select, Input, Button, DatePicker } from 'antd'
+import moment from 'moment'
 import i18n from '../../i18n'
 
 import { TopoObj } from '../../constants'
 
 import './style.css'
+
+function range (start, end) {
+  const result = []
+  for (let i = start; i < end; i++) {
+    result.push(i)
+  }
+  return result
+}
+
+function disabledDate (current) {
+  // Can not select days before today and today
+  return current && current < moment().endOf('day')
+}
+
+function disabledDateTime () {
+  return {
+    disabledHours: () => range(0, 24).splice(4, 20),
+    disabledMinutes: () => range(30, 60),
+    disabledSeconds: () => [ 55, 56 ],
+  }
+}
+
+function disabledRangeTime (_, type) {
+  if (type === 'start') {
+    return {
+      disabledHours: () => range(0, 60).splice(4, 20),
+      disabledMinutes: () => range(30, 60),
+      disabledSeconds: () => [ 55, 56 ],
+    }
+  }
+  return {
+    disabledHours: () => range(0, 60).splice(20, 4),
+    disabledMinutes: () => range(0, 31),
+    disabledSeconds: () => [ 55, 56 ],
+  }
+}
 
 export default class ReportMapModal extends React.Component {
   static propTypes = {
@@ -41,58 +78,28 @@ export default class ReportMapModal extends React.Component {
           onClose={onClose}
         >
 
-          <div>
-            <div>
+          <div className='content'>
+            <div className='input-name'>
               <div>{i18n.NAME_OF_DOCUMENT}</div>
               <Input value={'name'} />
             </div>
 
-            <div>
+            <div className='input-as-of'>
               <div>{i18n.AS_OF}</div>
+              <DatePicker
+                format="DD.MM.YYYY HH:mm"
+                // disabledDate={disabledDate}
+                // disabledTime={disabledDateTime}
+                showTime={{ defaultValue: moment('00:00', 'HH:mm') }}
+                style={{ minWidth: '300px' }}
+              />
             </div>
 
-            <div>
-              <Button>{i18n.YES}</Button>
-              <Button>{i18n.NO}</Button>
+            <div className='buttons'>
+              <Button style={{ minWidth: '100px' }}>{i18n.YES}</Button>
+              <Button style={{ minWidth: '100px' }}>{i18n.NO}</Button>
             </div>
           </div>
-          {/* <FocusTrap>onChange={handleChange
-            {objectsCount
-              ? <div className='objInfoContainer'>
-                <div className='elementListContainer'>
-                  {features.map((element, index) => <div
-                    key={index}
-                    className={`element ${index === selectedItem ? 'active' : ''}`}
-                    onClick={() => selectTopographicItem(index)}
-                  >
-                    {element.properties[ TopoObj.UKR_NAME ] ||
-                    element.properties[ TopoObj.PROPER_NAME ] ||
-                    element.properties[ TopoObj.OBJECT_TYPE ]}
-                  </div>)}
-                </div>
-                <div className='propertiesContainer'>
-                  <div className='mainPropertiesContainer'>
-                    <div
-                      className='mainProperties'>{`${TopoObj.OBJECT_TYPE}: ${features[ selectedItem ].properties[ TopoObj.OBJECT_TYPE ]}`}</div>
-                    <div
-                      className='mainProperties'>{`${TopoObj.TOPCODE}: ${features[ selectedItem ].properties[ TopoObj.TOPCODE ]}`}</div>
-                    <div
-                      className='mainProperties'>{`${TopoObj.POINT_COORDINATE}: ${location.lat} ${location.lng}`}</div>
-                  </div>
-                  <div className='secondaryPropertiesContainer'>
-                    {Object.keys(features[ selectedItem ].properties)
-                      .filter((item) =>
-                        item !== TopoObj.OBJECT_TYPE && item !== TopoObj.TOPCODE)
-                      .map((item, index) =>
-                        <div className='secondaryProperties' key={index}>
-                          {`${item}: ${features[ selectedItem ].properties[ item ]}`}
-                        </div>)
-                    }
-                  </div>
-                </div>
-              </div>
-              : <div className='elementListContainer'>{i18n.NO_OBJECTS}</div>}
-          </FocusTrap> */}
         </Wrapper>
       </div>
     )
