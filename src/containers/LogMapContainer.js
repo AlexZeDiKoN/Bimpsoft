b3a7d785bf73d14a027ad6ddcab698963a6e7975
yurii.khomenko <y.khomenko@iqusion.com>
2020-05-27 12:24:55 +0300

    work commit

--- src/containers/LogMapContainer.js
+++ src/containers/LogMapContainer.js
@@ -5,8 +5,9 @@ import { canEditSelector } from '../store/selectors'
 const mapStateToProps = ({ webMap, orgStructures }) => {
   const undoRecords = webMap.get('undoRecords').toArray()
   const objects = webMap.get('objects')
+  const undoPosition = webMap.get('undoPosition')
 
-  const userEvents = undoRecords.map(({ changeType, id }) => {
+  const userEvents = undoRecords.map(({ changeType, id, timestamp }) => {
     // const record = objects.get(id)
 
     // const objectsWithUnit = objects.filter(({ unit }) => unit === id)
@@ -14,12 +15,16 @@ const mapStateToProps = ({ webMap, orgStructures }) => {
     // const unitId = webMap.objects[id].unit
     // objects.getIn([ 'objects', id ])
     // record.get('unit')
+    // console.log('-------------------000', id)
+
     const objectById = objects.get(id)
 
     let objFullName = ''
     if (objectById) {
       const unitId = objectById.get('unit')
+      // console.log('-------------------111', unitId)
       const unit = orgStructures.unitsById[unitId]
+      // console.log('-------------------222', unit)
       objFullName = unit ? unit.fullName : ''
     }
 
@@ -30,12 +35,15 @@ const mapStateToProps = ({ webMap, orgStructures }) => {
     // const objFullName = orgStructures.unitsById[unitId].fullName
 
     const event = `${changeType} ${objFullName}`
-    console.log('-------------------8888', event)
+    // console.log('-------------------8888', event, undoPosition, undoRecords.length)
 
     return {
-
+      event,
+      timestamp,
     }
   })
+
+  console.log('------------------>>>>>', userEvents)
 
   return {
     userEvents: 1, // canEditSelector(state),
