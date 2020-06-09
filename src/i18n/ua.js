f2414c10a0ae3861e8f0969c2cc5d83d4f1a93b5
Тумак Олександр <tumak.aleksandr@iqusion.com>
2020-06-09 12:12:05 +0300

    = small fix

--- src/i18n/ua.js
+++ src/i18n/ua.js
@@ -68,7 +68,7 @@ export const SHOW_UNBOUND_OBJECTS = 'Показати неприв\'язані �
 export const EXTRA_FUNCTIONS = 'Додаткові функції'
 export const CREATE_REPORT_MAP = 'Створити звітну карту?'
 export const NAME_OF_DOCUMENT = 'Назва документа (обовязково)'
-export const AS_OF = 'Станом на(обовязково)'
+export const AS_OF = 'Станом на (обовязково)'
 export const REPORTING_CARD = 'Звітна карта'
 
 export const SHAPE_POLYLINE = 'Ломана лінія'
