7e29221864567c92e35c18bab60d4e48dbd4d839
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-08-20 10:57:44 +0300

    Исключены из проверки объекты без выбранного подразделения

--- src/components/menu/SelectionButtons/index.js
+++ src/components/menu/SelectionButtons/index.js
@@ -56,6 +56,7 @@ export default class SelectionButtons extends React.Component {
   }
 
   // проверка объекта при вставке на дублирование
+  // из проверки исключаются объекты не привязанные к подрпзделению
   onPasteObject = () => {
     const { onPasteError, onPaste, clipboard, objectsMap, layerId, orgStructures } = this.props
     const doubleObjects = clipboard.map((object) => {
