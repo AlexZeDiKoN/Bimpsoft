7d8128a3298efb59568a51e550e0efa545b50a66
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-08-28 14:40:43 +0300

    исправлены замечания

--- src/components/SelectionForm/parts/WithAmplifiers.js
+++ src/components/SelectionForm/parts/WithAmplifiers.js
@@ -3,7 +3,6 @@ import { Input, Tooltip } from 'antd'
 import { components } from '@DZVIN/CommonComponents'
 import i18n from '../../../i18n'
 import { amps } from '../../../constants/symbols'
-import NumberControl from '../../common/NumberControl'
 import { MAX_LENGTH_TEXT_AMPLIFIERS } from './WithPointAmplifiers'
 import './withAmplifiersStyle.css'
 
@@ -17,6 +16,7 @@ const PAIRS_DEFAULT = [
 
 export const PATH_AMPLIFIERS = [ 'attributes', 'pointAmplifier' ]
 export const TYPE_AMPLIFIER_NUM = 'num'
+export const TYPE_AMPLIFIER_INTEGER = 'integer'
 export const TYPE_AMPLIFIER_TEXT = 'text'
 
 const WithAmplifiers = (Component) => class AmplifiersComponent extends Component {
@@ -45,49 +45,71 @@ const WithAmplifiers = (Component) => class AmplifiersComponent extends Componen
       <div className="amplifier-container__item">
         {amplifiersPairs.map(({ id, name, type, maxRows, maxNumber }) => (
           <div className="amplifier-container__itemWidth" key={id}>
-            {type !== TYPE_AMPLIFIER_NUM
-              ? <FormRow title={null}
-                label={svg ? <Tooltip
+            <FormRow title={null}
+              label={svg
+                ? <Tooltip
                   overlayClassName='shape-form-svg-tooltip'
                   mouseEnterDelay={1}
                   placement={'left'}
                   title={() => svg}>
                   {`${i18n.AMPLIFIER} "${name}"`}
-                </Tooltip> : `${i18n.AMPLIFIER} "${name}"`}>
-                {maxRows === 1
-                  ? <Input
-                    className={!canEdit ? 'modals-input-disabled' : ''}
-                    name={name}
-                    value={currentValue[id] ?? ''}
-                    readOnly={!canEdit}
-                    onChange={this.createAmplifierHandler(id, pathAmplifiers, simpleObject)}
-                    disabled={!canEdit}
-                    maxLength={id === amps.A
-                      ? MAX_LENGTH_TEXT_AMPLIFIERS.TEXT_MULTILINE
-                      : MAX_LENGTH_TEXT_AMPLIFIERS.TEXTAREA}
-                  />
-                  : <Input.TextArea
-                    value={currentValue[id] ?? ''}
-                    onChange={this.createAmplifierHandler(id, pathAmplifiers, simpleObject)}
-                    readOnly={!canEdit}
-                    className={!canEdit ? 'modals-input-disabled' : ''}
-                    rows={id === amps.A ? 6 : 1}
-                    autoSize={ maxRows ? { minRows: 1, maxRows: maxRows } : undefined}
-                    maxLength={id === amps.A
-                      ? MAX_LENGTH_TEXT_AMPLIFIERS.TEXT_MULTILINE
-                      : MAX_LENGTH_TEXT_AMPLIFIERS.TEXTAREA}
-                  />}
-              </FormRow>
-              : <FormRow label={`${name}`}>
-                <NumberControl
-                  name={id}
+                </Tooltip>
+                : `${i18n.AMPLIFIER} "${name}"`}>
+              {type === TYPE_AMPLIFIER_NUM
+                ? <Input.Number
+                  type="number"
+                  className="number-control-input"
                   disabled={!canEdit}
-                  value={Number(currentValue[id])}
-                  onChange={this.changeNumAmplifier(pathAmplifiers, simpleObject)}
-                  maxNumber={maxNumber}
+                  readOnly={!canEdit}
+                  step={1}
+                  min="0"
+                  max={maxNumber}
+                  value={currentValue[id] ?? ''}
+                  onChange={this.changeHandler}
                 />
-              </FormRow>
-            }
+                : type === TYPE_AMPLIFIER_INTEGER
+                  ? <Input.Integer
+                    type="number"
+                    className="number-control-input"
+                    disabled={!canEdit}
+                    readOnly={!canEdit}
+                    step={1}
+                    min="0"
+                    max={maxNumber}
+                    value={currentValue[id] ?? ''}
+                    onChange={this.changeHandler}
+                  />
+                  : maxRows === 1
+                    ? <Input
+                      className={!canEdit ? 'modals-input-disabled' : ''}
+                      name={name}
+                      value={currentValue[id] ?? ''}
+                      readOnly={!canEdit}
+                      onChange={this.createAmplifierHandler(id, pathAmplifiers, simpleObject)}
+                      disabled={!canEdit}
+                      maxLength={id === amps.A
+                        ? MAX_LENGTH_TEXT_AMPLIFIERS.TEXT_MULTILINE
+                        : MAX_LENGTH_TEXT_AMPLIFIERS.TEXTAREA}
+                    />
+                    : <Input.TextArea
+                      value={currentValue[id] ?? ''}
+                      onChange={this.createAmplifierHandler(id, pathAmplifiers, simpleObject)}
+                      readOnly={!canEdit}
+                      className={!canEdit ? 'modals-input-disabled' : ''}
+                      rows={id === amps.A ? 6 : 1}
+                      autoSize={ maxRows ? { minRows: 1, maxRows: maxRows } : undefined}
+                      maxLength={id === amps.A
+                        ? MAX_LENGTH_TEXT_AMPLIFIERS.TEXT_MULTILINE
+                        : MAX_LENGTH_TEXT_AMPLIFIERS.TEXTAREA}
+                    />}
+            </FormRow>
+            {/* <NumberControl */}
+            {/*  name={id} */}
+            {/*  disabled={!canEdit} */}
+            {/*  value={Number(currentValue[id])} */}
+            {/*  onChange={this.changeNumAmplifier(pathAmplifiers, simpleObject)} */}
+            {/*  maxNumber={maxNumber} */}
+            {/* /> */}
           </div>
         ))}
       </div>
