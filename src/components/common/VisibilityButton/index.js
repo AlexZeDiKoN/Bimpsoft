71a8df17140bf74b0a7ad030670de7dbe0aaf8db
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-09-07 11:45:04 +0300

    Исправлены параметры инициализации знака: Прохід в протитанковому мінному полі (шириною до 6 метрів)         code: '10032500002906000000'         + DZVIN-9832 Блокировка зеркального отражения для сложных линий.

--- src/components/common/VisibilityButton/index.js
+++ src/components/common/VisibilityButton/index.js
@@ -5,7 +5,7 @@ import { IButton, IconNames } from '@DZVIN/CommonComponents'
 import './style.css'
 
 const VisibilityButton = (props) => {
-  const { title, visible, onChange, isDark, disabled } = props
+  const { title, visible, onChange, isDark } = props
 
   const clickHandler = (event) => {
     // Prevent event from triggering in other wrappers
@@ -23,7 +23,6 @@ const VisibilityButton = (props) => {
     <div className={classNames.join(' ')}>
       <Tooltip title={title} placement='topRight'>
         <IButton
-          disabled={disabled}
           onClick={clickHandler}
           icon={IconNames.DARK_EYE_ON}
         />
@@ -35,7 +34,6 @@ const VisibilityButton = (props) => {
 VisibilityButton.propTypes = {
   title: PropTypes.string,
   visible: PropTypes.bool,
-  disabled: PropTypes.bool,
   isDark: PropTypes.bool,
   onChange: PropTypes.func,
 }
