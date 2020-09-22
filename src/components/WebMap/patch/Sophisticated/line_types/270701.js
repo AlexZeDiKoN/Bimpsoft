09b51222c35e0540e678b565fee444d81d57bd60
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-09-22 20:16:13 +0300

    Добавлен контроль вывода текстовых амплификаторов для 240701, 270701, Переработан вывод знака 270701

--- src/components/WebMap/patch/Sophisticated/line_types/270701.js
+++ src/components/WebMap/patch/Sophisticated/line_types/270701.js
@@ -8,7 +8,7 @@ import {
   getPointSize,
   drawLightning,
   drawWires,
-  drawDotted,
+  drawDotted, getFontSize,
 } from '../utils'
 
 // sign name: Мінне поле (мінне загородження)
@@ -17,8 +17,8 @@ import {
 
 const SMALL_TEXT_SIZE = 0.67
 const MINE_SIZE = 0.65 // коэффициент размера мин по отношению к точечным знакам
-const KF_WIDTH = 6.8
-const KF_HEIGHT = 3.2
+const KF_WIDTH = 3.4
+const KF_HEIGHT = 1.6
 const COUNT_DASH = 6
 
 lineDefinitions['270701'] = {
@@ -39,75 +39,90 @@ lineDefinitions['270701'] = {
   ],
 
   // Рендер-функція
-  render: (result, points) => {
+  render: (result, points, _, toPrint) => {
     const mineType = result.layer?.object?.attributes?.params?.mineType ?? MINE_TYPES.ANTI_TANK
     const controlType = result.layer?.object?.attributes?.params?.controlType ?? CONTROL_TYPES.UNCONTROLLED
     const dummy = result.layer?.object?.attributes?.params?.dummy ?? false
-    const amplifiers = result.layer?.object?.attributes?.pointAmplifier ?? { top: null, middle: null, bottom: null }
     const pO = points[0]
     // міни
     const mineSize = getPointSize(result.layer) * MINE_SIZE
-    const d = mineSize / 2
+    const widthR = mineSize * KF_WIDTH
+    const heightR = mineSize * KF_HEIGHT
     const size = mineSize * (mineType >= MINE_TYPES.MARINE ? 0.85 : 1)
-    let symbol = new Symbol(CODE_MINE_TYPES[mineType], { size }).asSVG()
-    result.amplifiers += `<g transform="translate(${pO.x - d * 3.2}, ${pO.y - d * 1.05})">${symbol}</g>`
-    result.amplifiers += `<g transform="translate(${pO.x + d * 1.15}, ${pO.y - d * 1.05})">${symbol}</g>`
+    const mine = new Symbol(CODE_MINE_TYPES[mineType], { size })
+    let symbol = mine.asSVG()
+    let anchor = mine.getAnchor()
+    result.amplifiers += `<g transform="translate(${pO.x - widthR / 3 - anchor.x}, ${pO.y - anchor.y})">${symbol}</g>`
+    result.amplifiers += `<g transform="translate(${pO.x + widthR / 3 - anchor.x}, ${pO.y - anchor.y})">${symbol}</g>`
     if (mineType === MINE_TYPES.VARIOUS_TYPES) {
-      symbol = new Symbol(CODE_MINE_TYPES[0], { size }).asSVG()
+      const mine = new Symbol(CODE_MINE_TYPES[0], { size })
+      symbol = mine.asSVG()
+      anchor = mine.getAnchor()
     }
-    result.amplifiers += `<g transform="translate(${pO.x - d * 1.05}, ${pO.y - d * 1.05})">${symbol}</g>`
+    result.amplifiers += `<g transform="translate(${pO.x - anchor.x}, ${pO.y - anchor.y})">${symbol}</g>`
 
-    drawRectangleCz(result, pO, d * KF_WIDTH, d * KF_HEIGHT)
+    drawRectangleCz(result, pO, widthR, heightR)
 
+    const offsetX = widthR / 2
+    const offsetY = heightR / 2
     // Керованість
-    if (controlType === 1) { // Керованість по радіо
-      const pN = { x: pO.x + 2 * d, y: pO.y - d * 5 }
-      const pK = { x: pO.x, y: pO.y - d * 1.25 }
+    if (controlType === 1) {
+      // Керованість по радіо
+      const pN = { x: pO.x + offsetX / 2, y: pO.y - offsetY * 3 }
+      const pK = { x: pO.x, y: pO.y - offsetY }
       drawLightning(result, pN, pK)
-    } else if (controlType === 2) { // Керованість по проводам
-      const pN = { x: pO.x, y: pO.y + d * 4.5 }
-      const pK = { x: pO.x, y: pO.y + d * 1.25 }
+    } else if (controlType === 2) {
+      // Керованість по проводам
+      const pN = { x: pO.x, y: pO.y + offsetY * 3 }
+      const pK = { x: pO.x, y: pO.y + offsetY }
       drawWires(result, pK, pN)
     }
     // Макет/Хибний
     if (dummy) {
-      const pN = { x: pO.x - d * KF_WIDTH / 2, y: pO.y - d * KF_HEIGHT / 2 }
-      const pS = { x: pO.x, y: pO.y - d * (KF_WIDTH / 2 + KF_HEIGHT / 2) }
-      const pK = { x: pO.x + d * KF_WIDTH / 2, y: pO.y - d * KF_HEIGHT / 2 }
+      const pN = { x: pO.x - offsetX, y: pO.y - offsetY }
+      const pS = { x: pO.x, y: pO.y - offsetX - offsetY }
+      const pK = { x: pO.x + offsetX, y: pO.y - offsetY }
       drawDotted(result, [ pN, pS ], [ 1, 1 ], COUNT_DASH)
       drawDotted(result, [ pS, pK ], [ 1, 1 ], COUNT_DASH)
     }
-    // Ампліфікатор «N»
-    if (amplifiers.middle && amplifiers.middle.length !== 0) {
-      const pAR = { x: pO.x + d * 3.6, y: pO.y }
-      const pAL = { x: pO.x - d * 3.6, y: pO.y }
-      drawText(result, pAR, 0, amplifiers.middle, 1, 'start')
-      drawText(result, pAL, 0, amplifiers.middle, 1, 'end')
-    }
-    // Ампліфікатор «H1» «H2»
-    if (amplifiers.top && amplifiers.top.length !== 0) {
-      drawText(
-        result,
-        { x: pO.x, y: pO.y - d * 1.45 },
-        0,
-        amplifiers.top ?? '',
-        SMALL_TEXT_SIZE,
-        'middle',
-        null,
-        'after-edge',
-      )
-    }
-    if (amplifiers.bottom && amplifiers.bottom.length !== 0) {
-      drawText(
-        result,
-        { x: pO.x, y: pO.y + d * 1.4 },
-        0,
-        amplifiers.bottom ?? '',
-        SMALL_TEXT_SIZE,
-        'middle',
-        null,
-        'before-edge',
-      )
+
+    if (result.layer?.options?.showAmplifiers || toPrint) {
+      const amplifiers = result.layer?.object?.attributes?.pointAmplifier ?? { top: null, middle: null, bottom: null }
+      // Ампліфікатор «N»
+      const fontSize = getFontSize(result.layer)
+      if (amplifiers.middle && amplifiers.middle.length !== 0) {
+        const offset = offsetX + fontSize / 8
+        const pAR = { x: pO.x + offset, y: pO.y }
+        const pAL = { x: pO.x - offset, y: pO.y }
+        drawText(result, pAR, 0, amplifiers.middle, 1, 'start')
+        drawText(result, pAL, 0, amplifiers.middle, 1, 'end')
+      }
+      // Ампліфікатор «H1» «H2»
+      const offset = offsetY + fontSize / 8 * SMALL_TEXT_SIZE
+      if (amplifiers.top && amplifiers.top.length !== 0) {
+        drawText(
+          result,
+          { x: pO.x, y: pO.y - offset },
+          0,
+          amplifiers.top ?? '',
+          SMALL_TEXT_SIZE,
+          'middle',
+          null,
+          'text-after-edge',
+        )
+      }
+      if (amplifiers.bottom && amplifiers.bottom.length !== 0) {
+        drawText(
+          result,
+          { x: pO.x, y: pO.y + offset },
+          0,
+          amplifiers.bottom ?? '',
+          SMALL_TEXT_SIZE,
+          'middle',
+          null,
+          'text-before-edge',
+        )
+      }
     }
   },
 }
