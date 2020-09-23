a3ee9de7af4026cd19490a51a06c0885fdba179a
Oleksandr Tumak <tumak.oleksandr@gmail.com>
2020-09-23 17:09:12 +0300

    Resolves DZVIN-10085

--- src/components/WebMap/Tactical.js
+++ src/components/WebMap/Tactical.js
@@ -215,9 +215,8 @@ function createOlovo (data, layer, initMap) {
   const box = initMap.getBounds().pad(-0.4)
   const { directions, zones, start, title } = data.attributes.params
   let geometry = data.geometry.toJS()
-  if (directions + 1 !== geometry[0].length || zones + 1 !== geometry[0][0].length || (
-    layer && (layer.options.directions !== directions || layer.options.zones !== zones)
-  )) {
+
+  if (directions + 1 !== geometry[0].length || zones + 1 !== geometry[0][0].length) {
     if (layer) {
       const index = layer.map.objects.indexOf(layer)
       if (index >= 0) {
@@ -229,6 +228,7 @@ function createOlovo (data, layer, initMap) {
     geometry = generateGeometry(zones, directions, box)
   }
   const [ eternals, directionSegments, zoneSegments ] = geometry
+
   if (layer) {
     layer._map = initMap
     layer.updateProps(
