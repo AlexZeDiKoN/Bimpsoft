cea24ddb8e705094756727037549747e3dc9fb18
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-09-28 17:28:50 +0300

    Добавлено ограничение на количество вводимых строк в поля амплификаторов

--- src/constants/InputText.js
+++ src/constants/InputText.js
@@ -9,3 +9,9 @@ export const MAX_LENGTH_TEXT = {
   TEXT_DIRECTION: 40, // для назв напрямку
   NAME_REPORT_MAP: 300, // кількість символів для назви звітної карти
 }
+
+// ограничения на количество вводимых строк в соответствующие поля на формах
+export const MAX_ROW = {
+  POINT_AMP: 3,
+  INTERMEDIATE_AMP: 3,
+}
