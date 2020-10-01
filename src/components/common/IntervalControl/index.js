d7a7faa44488efecb123aa19fad3164740810eca
Samoilenko Oleksii <oleksii.samoilenko@iqusion.com>
2020-10-01 10:39:12 +0300

    DatePicker заменен на компонент из CommonComponents

--- src/components/common/IntervalControl/index.js
+++ src/components/common/IntervalControl/index.js
@@ -1,102 +1,83 @@
 import React from 'react'
-import { DatePicker, Tooltip } from 'antd'
+import { Tooltip } from 'antd'
 import PropTypes from 'prop-types'
-import { TIME_FORMAT, DATE_TIME_FORMAT } from '../../../constants/formats'
+import DatePicker from '@DZVIN/CommonComponents/build/components/inputs/DatePicker'
+import { DATE_TIME_FORMAT } from '../../../constants/formats'
 import i18n from '../../../i18n'
 
 import './style.css'
 
-const idStart = 'dataPickerStart'
-const idEnd = 'dataPickerEnd'
-
-let addId = 0
-
 export default class IntervalControl extends React.Component {
   constructor (props) {
     super(props)
-    addId += 1
-    this.idDataEnd = idEnd + addId
-    this.idDataStart = idStart + addId
     this.state = {
       dateStart: this.props.from,
       dateEnd: this.props.to,
     }
   }
 
-  disabledDateAfter = (current) => {
-    if (this.state.dateEnd === null) {
-      return false
+  ValidDateAfter = (current) => {
+    if (this.state.dateStart === null) {
+      return true
     }
 
     return current.isAfter(this.state.dateEnd)
   }
 
-  disabledDateBefore = (current) => {
-    if (this.state.dateStart === null) {
-      return false
+  ValidDateBefore = (current) => {
+    if (this.state.dateEnd === null) {
+      return true
     }
 
     return current.isBefore(this.state.dateStart)
   }
 
   onChangeFrom = (e) => {
-    e && e.second && e.second(0) // сброс секунд при выборе интервала
-    this.setState({ dateStart: e })
-    this.props.onChangeFrom(e)
+    this.setState({ dateStart: e.target.value })
+    this.props.onChangeFrom(e.target.value)
   }
 
   onChangeTo = (e) => {
-    this.setState({ dateEnd: e })
-    this.props.onChangeTo(e)
-  }
-
-  onOk = (id) => {
-    var event = new MouseEvent('mouseout', {
-      'bubbles': true,
-      'cancelable': true,
-    })
-    const cb = document.getElementById(id)
-    if (cb) {
-      cb.dispatchEvent(event)
-    }
+    this.setState({ dateEnd: e.target.value })
+    this.props.onChangeTo(e.target.value)
   }
 
   render () {
-    return (
-      <div className='interval-control'>
-        <span>{i18n.PERIOD_FROM}</span>
-        <Tooltip title={i18n.PERIOD_START} placement='topRight'>
+    return <div className='interval-control'>
+      <span>{i18n.PERIOD_FROM}</span>
+      <Tooltip
+        title={i18n.PERIOD_START}
+        placement='topRight'>
+        <div className={'calendar-picker'}>
           <DatePicker
-            id={this.idDataStart}
             value={this.state.dateStart}
-            style={{ minWidth: 'auto' }}
-            showTime={{ format: TIME_FORMAT }}
+            showTime={true}
             format={DATE_TIME_FORMAT}
             onChange={this.onChangeFrom}
-            onOk={() => this.onOk(this.idDataStart)}
             placeholder={''}
-            disabledDate={this.disabledDateAfter}
-          />
-        </Tooltip>
-        <span>{i18n.PERIOD_TO}</span>
-        <Tooltip
-          title={i18n.PERIOD_END}
-          placement='topRight'
-        >
+            isValidDate={this.ValidDateBefore}>
+          </DatePicker>
+        </div>
+      </Tooltip>
+      <span>{i18n.PERIOD_TO}</span>
+      <Tooltip
+        title={i18n.PERIOD_END}
+        placement='topRight'
+      >
+        <div className={'calendar-picker'}>
           <DatePicker
-            id={this.idDataEnd}
+            className='calendar-picker'
             value={this.state.dateEnd}
             style={{ minWidth: 'auto' }}
-            showTime={{ format: TIME_FORMAT }}
+            showTime={true}
             format={DATE_TIME_FORMAT}
             onChange={this.onChangeTo}
-            onOk={() => this.onOk(this.idDataEnd)}
             placeholder={''}
-            disabledDate={this.disabledDateBefore}
+            isValidDate={this.ValidDateAfter}
           />
-        </Tooltip>
-      </div>
-    )
+        </div>
+      </Tooltip>
+    </div>
   }
 }
 
