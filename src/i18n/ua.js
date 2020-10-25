8f30905ed0e3339c9b683e95b832493281eed724
barhylevych oleksandr <o.barhylevych@iqusion.com>
2020-10-26 01:31:25 +0200

    DZVIN

--- src/i18n/ua.js
+++ src/i18n/ua.js
@@ -510,7 +510,6 @@ export const UNDO = 'Скасувати останню дію'
 export const REDO = 'Повторити останню скасовану дію'
 
 export const ELEVATION_PROFILE = 'Профіль висот'
-export const HEIGHT = 'Висота'
 export const HEIGHT_2 = 'Висота (м)'
 export const PATH_LENGTH = 'Довжина шляху (м)'
 export const DIRECT_VISIBILITY = 'Пряма видимість'
