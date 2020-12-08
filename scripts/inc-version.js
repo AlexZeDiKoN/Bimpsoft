bcc5b2c9a17ce4825ef67b4ba9a209c2690c3455
Олександр <tumak.oleksandr@gmail.com>
2020-12-08 23:04:51 +0200

    + CI

--- scripts/inc-version.js
+++ scripts/inc-version.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
-
-console.info('Incrementing build number...')
+const path = require('path')
 
 try {
-  const major = (process.env[`CURRENT_VERSION`] || '').split('.')
-  const metadata = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+  const major = (process.env.CURRENT_VERSION || '').split('.')
+  console.info('Incrementing build number...')
+  const packageJsonPath = path.resolve(__dirname, '..', 'package.json')
+  const metadata = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
   const parts = metadata.version.split('.')
   if (parts.length > 2) {
     if (major.length === 2) {
@@ -14,12 +15,8 @@ try {
     parts[parts.length - 1] = parseInt(parts[parts.length - 1]) + 1
   }
   metadata.version = parts.join('.')
-  fs.writeFileSync('package.json', JSON.stringify(metadata, null, 2))
+  fs.writeFileSync(packageJsonPath, JSON.stringify(metadata, null, 2))
   console.info(`Current build number: ${metadata.version}`)
-  fs.writeFileSync('./src/version.json', JSON.stringify({
-    version: metadata.version,
-    warning: 'Do not modify this file manually!',
-  }, null, 2))
 } catch (err) {
   console.error(err)
 }
